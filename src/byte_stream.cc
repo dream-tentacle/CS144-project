45789c4bac7094ade4906fedffce5ccd1551fe71
221240060-Xiong Juncheng <221240060@smail.nju.edu.cn>
2024-09-13 21:07:23 +0800

    Complete lab0

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -2,64 +2,69 @@
 
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream(uint64_t capacity) : capacity_(capacity), buffer_("") {}
 
-bool Writer::is_closed() const
-{
+bool Writer::is_closed() const {
   // Your code here.
-  return {};
+  return closed_;
 }
 
-void Writer::push( string data )
-{
+void Writer::push(string data) {
   // Your code here.
-  (void)data;
+  // push the data to the stream, but only as much as available capacity allows.
+  long unsigned int bytes_can_be_pushed = capacity_ - buffer_.size();
+  if (bytes_can_be_pushed < data.size()) {
+    buffer_ += data.substr(0, bytes_can_be_pushed);
+    bytes_pushed_ += bytes_can_be_pushed;
+  } else {
+    buffer_ += data;
+    bytes_pushed_ += data.size();
+  }
   return;
 }
 
-void Writer::close()
-{
+void Writer::close() {
   // Your code here.
+  closed_ = true;
 }
 
-uint64_t Writer::available_capacity() const
-{
+uint64_t Writer::available_capacity() const {
   // Your code here.
-  return {};
+  return capacity_ - buffer_.size();
 }
 
-uint64_t Writer::bytes_pushed() const
-{
+uint64_t Writer::bytes_pushed() const {
   // Your code here.
-  return {};
+  return bytes_pushed_;
 }
 
-bool Reader::is_finished() const
-{
+bool Reader::is_finished() const {
   // Your code here.
-  return {};
+  return closed_ && buffer_.empty(); // closed and fully popped
 }
 
-uint64_t Reader::bytes_popped() const
-{
+uint64_t Reader::bytes_popped() const {
   // Your code here.
-  return {};
+  return bytes_popped_;
 }
 
-string_view Reader::peek() const
-{
+string_view Reader::peek() const {
   // Your code here.
-  return {};
+  return string_view(buffer_);
 }
 
-void Reader::pop( uint64_t len )
-{
+void Reader::pop(uint64_t len) {
   // Your code here.
-  (void)len;
+  if (len > buffer_.size()) {
+    bytes_popped_ += buffer_.size();
+    buffer_.clear();
+  } else {
+    bytes_popped_ += len;
+    buffer_ = buffer_.substr(len);
+  }
 }
 
-uint64_t Reader::bytes_buffered() const
-{
+uint64_t Reader::bytes_buffered() const {
   // Your code here.
-  return {};
+  return buffer_.size();
 }
