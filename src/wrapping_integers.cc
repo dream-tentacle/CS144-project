48593dee594cc1c2a5bdd5dd58cdfc1de27c4000
221240060-Xiong Juncheng <221240060@smail.nju.edu.cn>
2024-10-11 20:39:48 +0800

    delete unuseful notes

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -14,12 +14,6 @@ uint64_t Wrap32::unwrap(Wrap32 zero_point, uint64_t checkpoint) const {
   uint64_t approx = checkpoint & ~0xFFFFFFFFUL;
   uint32_t diff = raw_value_ - zero_point.raw_value_;
   uint64_t re = approx + static_cast<uint64_t>(diff);
-  // if (checkpoint == 3 * (1UL << 32) && diff == UINT32_MAX - 1) {
-  //   string tmp = "checkpoint: " + to_string(checkpoint) +
-  //                ", diff: " + to_string(diff) + ", re: " + to_string(re) +
-  //                ", approx: " + to_string(approx);
-  //   throw runtime_error(tmp);
-  // }
   if (re > checkpoint) {
     if (re < (1LL << 32)) {
       return re;
